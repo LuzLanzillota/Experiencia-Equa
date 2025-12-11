d81be443322e5905942b02f5116ac68e11eac5e7
marialuzlanzillota <160551722+LuzLanzillota@users.noreply.github.com>
2025-12-11 10:22:44 -0300

    No se rompe más el contador

---
+++ Assets/ResetAlIniciar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ResetAlIniciar : MonoBehaviour
+{
+    private void Start()
+    {
+        // 🔥 Esto solo se ejecuta al entrar al menú principal
+        PlayerPrefs.SetInt("EscenaAnterior", -1);
+        PlayerPrefs.Save();
+    }
+}
+
